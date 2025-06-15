9a2c131ffd684b78d010bbe9cba161567abab997
kamsspace <h.u.kamolov@icloud.com>
2025-06-15 16:45:03 +0500

    Created the updateImage method and the location for all images to be stored with fileName randomly renamed with UUID

--- src/main/java/com/kamsspace/ecommerceapp/service/ProductServiceImpl.java
+++ src/main/java/com/kamsspace/ecommerceapp/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kamsspace.ecommerceapp.service;
 
+import com.kamsspace.ecommerceapp.exception.APIException;
 import com.kamsspace.ecommerceapp.exception.ResourceNotFoundException;
 import com.kamsspace.ecommerceapp.model.Category;
 import com.kamsspace.ecommerceapp.model.Product;
@@ -9,15 +10,12 @@ import com.kamsspace.ecommerceapp.repositories.CategoryRepository;
 import com.kamsspace.ecommerceapp.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -31,18 +29,38 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
+
     @Override
     public ProductDTO addProduct(ProductDTO productDTO, Long categoryId) {
+        // Check if product already present or not
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
 
-        Product product = modelMapper.map(productDTO, Product.class);
-        product.setImage("default.png");
-        product.setCategory(category);
-        double specialPrice = product.getPrice() - (product.getDiscount() * 0.01) * product.getPrice();
-        product.setSpecialPrice(specialPrice);
-        Product savedProduct = productRepository.save(product);
-        return modelMapper.map(savedProduct, ProductDTO.class);
+        boolean isProductNotPresent = true;
+        List<Product> products = category.getProducts();
+        for (Product value : products) {
+            if (value.getProductName().equals(productDTO.getProductName())) {
+                isProductNotPresent = false;
+                break;
+            }
+        }
+
+        if (isProductNotPresent) {
+            Product product = modelMapper.map(productDTO, Product.class);
+            product.setImage("default.png");
+            product.setCategory(category);
+            double specialPrice = product.getPrice() - (product.getDiscount() * 0.01) * product.getPrice();
+            product.setSpecialPrice(specialPrice);
+            Product savedProduct = productRepository.save(product);
+            return modelMapper.map(savedProduct, ProductDTO.class);
+        } else {
+            throw new APIException("Product already exists");
+        }
     }
 
     @Override
@@ -51,6 +69,10 @@ public class ProductServiceImpl implements ProductService {
         List<ProductDTO> productDTOS = products.stream()
                 .map(product -> modelMapper.map(product, ProductDTO.class))
                 .toList();
+
+        if (products.isEmpty()) {
+            throw new APIException("No Products Exist.");
+        }
 
         ProductResponse productResponse = new ProductResponse();
         productResponse.setContent(productDTOS);
@@ -123,8 +145,8 @@ public class ProductServiceImpl implements ProductService {
 
         // upload image to server (/image directory for now)
         // get the filename of uploaded image
-        String path = "images/";
-        String fileName = uploadImage(path, image);
+
+        String fileName = fileService.uploadImage(path, image);
 
         // updating the new filename to the product
         productFromDb.setImage(fileName);
@@ -134,28 +156,6 @@ public class ProductServiceImpl implements ProductService {
 
         // return DTO after mapping product to DTO
         return modelMapper.map(updatedProduct, ProductDTO.class);
-    }
-
-    private String uploadImage(String path, MultipartFile file) throws IOException {
-        // file names of current / original file
-        String originalFileName = file.getOriginalFilename();
-
-        // generate a unique file name (random uuid)
-        String randomId = UUID.randomUUID().toString();
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        String filePath = path + File.separator + fileName;
-
-        // check if path exists and create
-        File folder = new File(path);
-        if (!folder.exists()) {
-            folder.mkdir();
-        }
-
-        // upload to server
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-
-        // returning file name
-        return fileName;
     }
 
 }
